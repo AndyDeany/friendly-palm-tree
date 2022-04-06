c616904d5899cdb40aff474964217b1087f13cdb
Andrew Dean <oneandydean@hotmail.com>
2022-04-06 19:57:12 +0100

    Fix bad expects syntax

--- spec/ferrets_spec.py
+++ spec/ferrets_spec.py
@@ -1,59 +1,60 @@
 """File containing unit tests for the functionality of the /ferrets endpoint."""
 # pylint: disable=invalid-name
 
-from expects import expect
+from expects import expect, equal
 from mamba import context, it
 import requests
 
-from spec.helper import *
+from spec.helper import (BASE_URL,
+                         get_ciri_dict, get_georgia_dict, get_slinky_dict, get_cerberus_dict)
 
 
 with context("/ferrets base endpoint"):
     with it("should return all ferrets, in alphabetical order by name"):
         response = requests.get(BASE_URL + "/ferrets")
-        expect(response.content).to_equal({"ferrets": [get_cerberus_dict(), get_ciri_dict(),
-                                                       get_georgia_dict(), get_slinky_dict()]})
-        expect(response.status_code).to_equal(200)
+        expect(response.json()).to(equal({"ferrets": [get_cerberus_dict(), get_ciri_dict(),
+                                                       get_georgia_dict(), get_slinky_dict()]}))
+        expect(response.status_code).to(equal(200))
 
     with it("should return all ferrets that match a given search parameter"):
         response = requests.get(BASE_URL + "/ferrets", params={"color": "Chocolate"})
-        expect(response.content).to_equal({"ferrets": [get_cerberus_dict(), get_georgia_dict()]})
-        expect(response.status_code).to_equal(200)
+        expect(response.json()).to(equal({"ferrets": [get_cerberus_dict(), get_georgia_dict()]}))
+        expect(response.status_code).to(equal(200))
 
     with it("should gracefully return no ferrets when none match a given search parameter"):
         response = requests.get(BASE_URL + "/ferrets", params={"color": "Silver"})
-        expect(response.content).to_equal({"ferrets": []})
-        expect(response.status_code).to_equal(200)
+        expect(response.json()).to(equal({"ferrets": []}))
+        expect(response.status_code).to(equal(200))
 
     with it("should return an appropriate 400 message if an unknown search parameter is given"):
         response = requests.get(BASE_URL + "/ferrets", params={"length": "32"})
-        expect(response.content).to_equal({"message": "Bad Request. Unknown parameter 'length'."})
-        expect(response.status_code).to_equal(400)
+        expect(response.json()).to(equal({"message": "Bad Request. Unknown parameter 'length'."}))
+        expect(response.status_code).to(equal(400))
 
     with it("should do this even if a valid parameter is also given"):
         response = requests.get(BASE_URL + "/ferrets", params={"color": "Albino", "length": "32"})
-        expect(response.content).to_equal({"message": "Bad Request. Unknown parameter 'length'."})
-        expect(response.status_code).to_equal(400)
+        expect(response.json()).to(equal({"message": "Bad Request. Unknown parameter 'length'."}))
+        expect(response.status_code).to(equal(400))
 
     with it("should return 501 Not Implemented if a POST request is made"):
         noodle_json = {"name": "Noodle", "dob": "2022-02-26", "color": "Silver"}
         expected_message = "POST requests are not implemented for this resource."
         response = requests.post(BASE_URL + "/ferrets", json=noodle_json)
-        expect(response.content).to_equal({"message": expected_message})
-        expect(response.status_code).to_equal(501)
+        expect(response.json()).to(equal({"message": expected_message}))
+        expect(response.status_code).to(equal(501))
 
     with it("should return 403 Forbidden if a DELETE request is made"):
         noodle_json = {"name": "Noodle", "dob": "2022-02-26", "color": "Silver"}
         expected_message = "DELETE requests are not allowed on this resource."
         response = requests.delete(BASE_URL + "/ferrets", json=noodle_json)
-        expect(response.content).to_equal({"message": expected_message})
-        expect(response.status_code).to_equal(403)
+        expect(response.json()).to(equal({"message": expected_message}))
+        expect(response.status_code).to(equal(403))
 
     with it("should return 403 Forbidden if a PUT request is made"):
         expected_message = "PUT requests are not allowed on this resource."
         response = requests.put(BASE_URL + "/ferrets", json={})
-        expect(response.content).to_equal({"message": expected_message})
-        expect(response.status_code).to_equal(403)
+        expect(response.json()).to(equal({"message": expected_message}))
+        expect(response.status_code).to(equal(403))
 
 
 with context("/ferrets/{name} endpoint"):
@@ -63,29 +64,29 @@ with context("/ferrets/{name} endpoint"):
                                       ("Cerberus", get_cerberus_dict),
                                       ("Georgia", get_georgia_dict)):
             response = requests.get(BASE_URL + f"/ferrets/{name}")
-            expect(response.content).to_equal(get_ferret_dict())
-            expect(response.status_code).to_equal(200)
+            expect(response.json()).to(equal(get_ferret_dict()))
+            expect(response.status_code).to(equal(200))
 
     with it("should do so in a case-insenstive manner"):
         for name in ("ciri", "CIRI", "cIrI"):
             response = requests.get(BASE_URL + f"/ferrets/{name}")
-            expect(response.content).to_equal(get_ciri_dict())
-            expect(response.status_code).to_equal(200)
+            expect(response.json()).to(equal(get_ciri_dict()))
+            expect(response.status_code).to(equal(200))
 
     with it("should return an appropriate 404 message if a requested ferret can't be found"):
         expected_message = "Ferret not found. No ferret with the name 'Noodle' exists."
         response = requests.get(BASE_URL + "/ferrets/Noodle")
-        expect(response.content).to_equal({"message": expected_message})
-        expect(response.status_code).to_equal(404)
+        expect(response.json()).to(equal({"message": expected_message}))
+        expect(response.status_code).to(equal(404))
 
     with it("should return 501 Not Implemented if a PUT request is made"):
         expected_message = "PUT requests are not implemented for this resource."
         response = requests.put(BASE_URL + "/ferrets/Ciri", json={"color": "Silver"})
-        expect(response.content).to_equal({"message": expected_message})
-        expect(response.status_code).to_equal(501)
+        expect(response.json()).to(equal({"message": expected_message}))
+        expect(response.status_code).to(equal(501))
 
     with it("should return 501 Not Implemented if a DELETE request is made"):
         expected_message = "DELETE requests are not implemented for this resource."
         response = requests.post(BASE_URL + "/ferrets/Ciri", json=noodle_json)
-        expect(response.content).to_equal({"message": expected_message})
-        expect(response.status_code).to_equal(501)
+        expect(response.json()).to(equal({"message": expected_message}))
+        expect(response.status_code).to(equal(501))
--- spec/helper.py
+++ spec/helper.py
@@ -5,7 +5,7 @@ from datetime import date as _date
 BASE_URL = "http://127.0.0.1:5000"
 
 
-def make_ferret_dict(ferret_name, dob, color):
+def _make_ferret_dict(ferret_name, dob, color):
     """Create the expect ferret dict for the ferret with the given information."""
     today = _date.today()
     return {
@@ -16,7 +16,7 @@ def make_ferret_dict(ferret_name, dob, color):
 
 
 # Create dicts when requests are made (instead of now) so age is accurate.
-get_ciri_dict = lambda: make_ferret_dict("Ciri", _date(2019, 9, 13), "Albino")
-get_slinky_dict = lambda: make_ferret_dict("Slinky", _date(2021, 4, 6), "Black Sable")
-get_cerberus_dict = lambda: make_ferret_dict("Cerberus", _date(2015, 12, 31), "Chocolate")
-get_georgia_dict = lambda: make_ferret_dict("Georgia", _date(2017, 1, 1), "Chocolate")
+get_ciri_dict = lambda: _make_ferret_dict("Ciri", _date(2019, 9, 13), "Albino")
+get_slinky_dict = lambda: _make_ferret_dict("Slinky", _date(2021, 4, 6), "Black Sable")
+get_cerberus_dict = lambda: _make_ferret_dict("Cerberus", _date(2015, 12, 31), "Chocolate")
+get_georgia_dict = lambda: _make_ferret_dict("Georgia", _date(2017, 1, 1), "Chocolate")
