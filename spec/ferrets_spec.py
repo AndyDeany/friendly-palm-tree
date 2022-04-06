a8ebc67585157eaace267fa2c35652cb6e4fdbc4
Andrew Dean <oneandydean@hotmail.com>
2022-04-06 18:03:11 +0100

    Add unit tests for the functionality of the /ferrets endpoint

---
+++ spec/ferrets_spec.py
@@ -0,0 +1,110 @@
+"""File containing unit tests for the functionality of the /ferrets endpoint."""
+
+from datetime import date
+
+from expects import *
+from mamba import context, it
+import requests
+
+
+BASE_URL = "http://127.0.01:5000"
+
+
+def make_ferret_dict(name, dob, color):
+    """Create the expect ferret dict for the ferret with the given information."""
+    today = date.today()
+    return {
+        "name": name,
+        "age": today.year - dob.year - ((today.month, today.day) < (dob.month, dob.day)),
+        "color": color,
+    }
+
+
+# Create dicts when requests are made (instead of now) so age is accurate.
+get_ciri_dict = lambda: make_ferret_dict("Ciri", date(2019, 9, 13), "Albino")
+get_slinky_dict = lambda: make_ferret_dict("Slinky", date(2021, 4, 6), "Black Sable")
+get_cerberus_dict = lambda: make_ferret_dict("Cerberus", date(2015, 12, 31), "Chocolate")
+get_georgia_dict = lambda: make_ferret_dict("Georgia", date(2017, 1, 1), "Chocolate")
+
+
+with context("/ferrets base endpoint"):
+    with it("should return all ferrets, in alphabetical order by name"):
+        response = requests.get(BASE_URL + "/ferrets")
+        expect(response.content).to_equal({"ferrets": [get_cerberus_dict(), get_ciri_dict(),
+                                                       get_georgia_dict(), get_slinky_dict()]})
+        expect(response.status_code).to_equal(200)
+
+    with it("should return all ferrets that match a given search parameter"):
+        response = requests.get(BASE_URL + "/ferrets", params={"color": "Chocolate"})
+        expect(response.content).to_equal({"ferrets": [get_cerberus_dict(), get_georgia_dict()]})
+        expect(response.status_code).to_equal(200)
+
+    with it("should gracefully return no ferrets when none match a given search parameter"):
+        response = requests.get(BASE_URL + "/ferrets", params={"color": "Silver"})
+        expect(response.content).to_equal({"ferrets": []})
+        expect(response.status_code).to_equal(200)
+
+    with it("should return an appropriate 400 message if an unknown search parameter is given"):
+        response = requests.get(BASE_URL + "/ferrets", params={"length": "32"})
+        expect(response.content).to_equal({"message": "Bad Request. Unknown parameter 'length'."})
+        expect(response.status_code).to_equal(400)
+
+    with it("should do this even if a valid parameter is also given"):
+        response = requests.get(BASE_URL + "/ferrets", params={"color": "Albino", "length": "32"})
+        expect(response.content).to_equal({"message": "Bad Request. Unknown parameter 'length'."})
+        expect(response.status_code).to_equal(400)
+
+    with it("should return 501 Not Implemented if a POST request is made"):
+        noodle_json = {"name": "Noodle", "dob": "2022-02-26", "color": "Silver"}
+        expected_message = "POST requests are not implemented for this resource."
+        response = requests.post(BASE_URL + "/ferrets", json=noodle_json)
+        expect(response.content).to_equal({"message": expected_message})
+        expect(response.status_code).to_equal(501)
+
+    with it("should return 403 Forbidden if a DELETE request is made"):
+        noodle_json = {"name": "Noodle", "dob": "2022-02-26", "color": "Silver"}
+        expected_message = "DELETE requests are not allowed on this resource."
+        response = requests.delete(BASE_URL + "/ferrets", json=noodle_json)
+        expect(response.content).to_equal({"message": expected_message})
+        expect(response.status_code).to_equal(403)
+
+    with it("should return 403 Forbidden if a PUT request is made"):
+        expected_message = "PUT requests are not allowed on this resource."
+        response = requests.put(BASE_URL + "/ferrets", json={})
+        expect(response.content).to_equal({"message": expected_message})
+        expect(response.status_code).to_equal(403)
+
+
+with context("/ferrets/{name} endpoint"):
+    with it("should return a specific ferret's information when given their name"):
+        for name, get_ferret_dict in (("Ciri", get_ciri_dict),
+                                      ("Slinky", get_slinky_dict),
+                                      ("Cerberus", get_cerberus_dict),
+                                      ("Georgia", get_georgia_dict)):
+            response = requests.get(BASE_URL + f"/ferrets/{name}")
+            expect(response.content).to_equal(get_ferret_dict())
+            expect(response.status_code).to_equal(200)
+
+    with it("should do so in a case-insenstive manner"):
+        for name in ("ciri", "CIRI", "cIrI"):
+            response = requests.get(BASE_URL + f"/ferrets/{name}")
+            expect(response.content).to_equal(get_ciri_dict())
+            expect(response.status_code).to_equal(200)
+
+    with it("should return an appropriate 404 message if a requested ferret can't be found"):
+        expected_message = "Ferret not found. No ferret with the name 'Noodle' exists."
+        response = requests.get(BASE_URL + f"/ferrets/Noodle")
+        expect(response.content).to_equal({"message": expected_message})
+        expect(response.status_code).to_equal(404)
+
+    with it("should return 501 Not Implemented if a PUT request is made"):
+        expected_message = "PUT requests are not implemented for this resource."
+        response = requests.put(BASE_URL + "/ferrets/Ciri", json={"color": "Silver"})
+        expect(response.content).to_equal({"message": expected_message})
+        expect(response.status_code).to_equal(501)
+
+    with it("should return 501 Not Implemented if a DELETE request is made"):
+        expected_message = "DELETE requests are not implemented for this resource."
+        response = requests.post(BASE_URL + "/ferrets/Ciri", json=noodle_json)
+        expect(response.content).to_equal({"message": expected_message})
+        expect(response.status_code).to_equal(501)
