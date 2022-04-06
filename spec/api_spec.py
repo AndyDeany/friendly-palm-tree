7766909efd63efdf9c9cf278abf01308b0bcb8eb
Andrew Dean <oneandydean@hotmail.com>
2022-04-06 23:48:44 +0100

    Add test for whether the API can handle concurrent calls. This test already passes

---
+++ spec/api_spec.py
@@ -0,0 +1,36 @@
+"""File containing unit tests for functionality of the API as a whole."""
+from threading import Thread
+from time import time
+
+from expects import expect, equal, be_below
+from mamba import context, it
+import requests
+
+from spec.helper import *
+
+
+response_dict = {}  # For passing info between threads
+
+
+def make_blocking_request():
+    """Make a request that is slow as to test that other requests made simultaneously succeed."""
+    response_dict["response"] = requests.delete(BASE_URL + "/ferrets")  # Takes 5 seconds
+
+
+with context("simultaenous requests"):
+    with it("should handle a second request whilst handling a blocking request"):
+        start_time = time()
+        blocking_process = Thread(target=make_blocking_request)
+        blocking_process.start()
+
+        response = requests.get(BASE_URL + "/ferrets/Ciri")
+        expect(response.json()).to(equal(get_ciri_dict()))
+        expect(response.status_code).to(equal(200))
+        # Check that the request was completed whilst the blocking request was ongoing (within 1s)
+        expect(time() - start_time).to(be_below(1))
+
+        blocking_process.join()
+        slow_response = response_dict["response"]
+        expected_json = {"message": "DELETE requests are not supported for this resource."}
+        expect(slow_response.json()).to(equal(expected_json))
+        expect(slow_response.status_code).to(equal(405))
